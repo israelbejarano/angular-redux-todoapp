cbede485ca8cb314c74397c9801a2cd596b2319b
israel <israel.bejarano.a@gmail.com>
2019-04-16 19:28:25 +0200

    impor de ReactiveForms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,10 @@ import { StoreModule } from '@ngrx/store';
 import { todoReducer } from './todo/todo.reducer';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
+// forms
+import { ReactiveFormsModule } from '@angular/forms';
+
+
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
@@ -28,6 +32,7 @@ import { environment } from '../environments/environment';
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     StoreModule.forRoot({ todos: todoReducer}),
     // sirve para con la extension en chrome ir depurando y ver los estados anteriores (25) solo en dev en prod no se ve
     StoreDevtoolsModule.instrument({
