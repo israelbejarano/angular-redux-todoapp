e549802e469d1dc33d023ef7797490458a5bd0b0
israel <israel.bejarano.a@gmail.com>
2019-04-16 19:23:01 +0200

    import y uso de store devtools y del store module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// ngrx
+import { StoreModule } from '@ngrx/store';
+import { todoReducer } from './todo/todo.reducer';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { TodoComponent } from './todo/todo.component';
@@ -8,6 +14,7 @@ import { TodosListComponent } from './todo/todos-list/todos-list.component';
 import { TodoItemComponent } from './todo/todo-item/todo-item.component';
 import { TodoFooterComponent } from './todo/todo-footer/todo-footer.component';
 import { TodoAddComponent } from './todo/todo-add/todo-add.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -20,7 +27,12 @@ import { TodoAddComponent } from './todo/todo-add/todo-add.component';
     TodoAddComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot({ todos: todoReducer}),
+    // sirve para con la extension en chrome ir depurando y ver los estados anteriores (25) solo en dev en prod no se ve
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production})
   ],
   providers: [],
   bootstrap: [AppComponent]
