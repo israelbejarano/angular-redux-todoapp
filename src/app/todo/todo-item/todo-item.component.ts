d4d05ccd1d6759bcce556e1b8d11eec9d45b89c1
israel <israel.bejarano.a@gmail.com>
2019-04-17 16:38:38 +0200

    accion de editar texto

--- src/app/todo/todo-item/todo-item.component.ts
+++ src/app/todo/todo-item/todo-item.component.ts
@@ -3,7 +3,7 @@ import { Todo } from '../model/todo.model';
 import { FormControl, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../app.reducers';
-import { ToggleTodoAction } from '../todo.actions';
+import { ToggleTodoAction, EditarTodoAction } from '../todo.actions';
 
 @Component({
   selector: 'app-todo-item',
@@ -39,6 +39,14 @@ export class TodoItemComponent implements OnInit {
 
   terminarEdicion() {
     this.editando = false;
+    if (this.txtInput.invalid) {
+      return;
+    }
+    if (this.txtInput.value === this.todo.texto) {
+      return;
+    }
+    const accion = new EditarTodoAction(this.todo.id, this.txtInput.value);
+    this.store.dispatch(accion);
   }
 
 }
