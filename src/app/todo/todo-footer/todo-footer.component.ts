c22c63a5eb891daf74d137cb290a60ff98ef8008
israel <israel.bejarano.a@gmail.com>
2019-04-17 18:11:40 +0200

    contador de tareas pendientes

--- src/app/todo/todo-footer/todo-footer.component.ts
+++ src/app/todo/todo-footer/todo-footer.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import * as fromFiltro from '../../filter/filter.actions';
 import { Store } from '@ngrx/store';
 import { AppState } from 'src/app/app.reducers';
+import { Todo } from '../model/todo.model';
 
 @Component({
   selector: 'app-todo-footer',
@@ -12,11 +13,13 @@ export class TodoFooterComponent implements OnInit {
 
   filtrosValidos: fromFiltro.filtrosValidos[] = ['todos', 'completados', 'pendientes'];
   filtroActual: fromFiltro.filtrosValidos;
+  pendientes: number;
 
   constructor(private store: Store<AppState>) { }
 
   ngOnInit() {
     this.store.subscribe(state => {
+      this.contarPendientes(state.todos);
       this.filtroActual = state.filtro;
     });
   }
@@ -24,6 +27,10 @@ export class TodoFooterComponent implements OnInit {
   cambiarFiltro(nuevoFiltro: fromFiltro.filtrosValidos) {
     const accion = new fromFiltro.SetFiltroAction(nuevoFiltro);
     this.store.dispatch(accion);
+  }
+
+  contarPendientes(todos: Todo[]) {
+    this.pendientes = todos.filter(todo => !todo.completado).length;
   }
 
 }
