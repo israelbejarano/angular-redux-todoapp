4e09983617c9cdf7af56bc7f278be7952c6d7cc1
israel <israel.bejarano.a@gmail.com>
2019-04-17 17:44:07 +0200

    uso de dos reducers a la vez en la app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 // ngrx
 import { StoreModule } from '@ngrx/store';
-import { todoReducer } from './todo/todo.reducer';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { appReducers } from './app.reducers';
 
 // forms
 import { ReactiveFormsModule } from '@angular/forms';
-
-
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
@@ -33,7 +31,7 @@ import { environment } from '../environments/environment';
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    StoreModule.forRoot({ todos: todoReducer}),
+    StoreModule.forRoot(appReducers),
     // sirve para con la extension en chrome ir depurando y ver los estados anteriores (25) solo en dev en prod no se ve
     StoreDevtoolsModule.instrument({
       maxAge: 25,
